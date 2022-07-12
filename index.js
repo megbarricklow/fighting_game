0e9b9f89e593900c0eb5faf10731e618e7c03c28
megbarricklow <meghan.barricklow@gmail.com>
2022-07-12 16:33:55 -0400

    Added player and enemy movement

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ class Sprite {
         this.position = position
         this.velocity = velocity
         this.height = 150
+        this.lastKey
     }
 
     draw() {
@@ -65,6 +66,12 @@ const keys = {
     },
     w: {
         pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
     }
 }
 
@@ -78,11 +85,20 @@ function animate() {
     enemy.update()
 
     player.velocity.x = 0
+    enemy.velocity.x = 0
 
+    // player movement
     if (keys.a.pressed && lastKey === 'a') {
         player.velocity.x = -1 
     } else if (keys.d.pressed && lastKey === 'd') {
         player.velocity.x = 1
+    }
+
+    // enemy movement
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -1 
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = 1
     }
 }
 
@@ -101,11 +117,24 @@ window.addEventListener('keydown', (event) => {
         case 'w':
             player.velocity.y = -10
             break
+            
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            enemy.velocity.y = -10
+            break
     }
 }) 
- 
 
 window.addEventListener('keyup', (event) => {
+
+    // player keys
     switch (event.key) {
         case 'd':
             keys.d.pressed = false
@@ -116,5 +145,17 @@ window.addEventListener('keyup', (event) => {
         case 'w':
             keys.w.pressed = false
             break
+
+    // enemy keys
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            break
+        case 'ArrowUp':
+            keys.ArrowUp.pressed = false
+            break
     }
 }) 
+
